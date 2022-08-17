cfeb66b46fd5d131e7538b9e3ce2de23de324c40
lditzel94 <lucianoditzel94@gmail.com>
2022-08-17 17:35:21 -0300

    - HealthyBurger -> itemizeHamburger override fixed - Hamburger -> unused getters and setter removed

--- src/main/java/org/core/HealthyBurger.java
+++ src/main/java/org/core/HealthyBurger.java
@@ -7,33 +7,24 @@ public class HealthyBurger extends Hamburger {
     private double healthyExtra2Price;
 
 
-    public HealthyBurger(String meat, double price) {
-        super("Healthy", meat, price, "Brown");
+    public HealthyBurger( String meat, double price ) {
+        super( "Healthy", meat, price, "Brown" );
     }
-    
-    public void addHealthyAddition1(String healthyExtra1Name,double healthyExtra1Price){
+
+    public void addHealthyAddition1( String healthyExtra1Name, double healthyExtra1Price ) {
         this.healthyExtra1Name = healthyExtra1Name;
         this.healthyExtra1Price = healthyExtra1Price;
     }
-    public void addHealthyAddition2(String healthyExtra2Name,double healthyExtra2Price){
+
+    public void addHealthyAddition2( String healthyExtra2Name, double healthyExtra2Price ) {
         this.healthyExtra2Name = healthyExtra2Name;
         this.healthyExtra2Price = healthyExtra2Price;
     }
 
     @Override
     public double itemizeHamburger() {
-        System.out.println("Hamburger{" +
-                        "name='" + getName() + '\'' +
-                        ", meat='" + getMeat() + '\'' +
-                        ", price=" + getPrice() +
-                        ", breadRollType='" + getBreadRollType() + '\'' +
-                        ", healthyExtra1Name=" + healthyExtra1Name +
-                        ", healthyExtra1Price=" + healthyExtra1Price +
-                        ", healthyExtra2Name=" + healthyExtra2Name +
-                        ", healthyExtra2Price=" + healthyExtra2Price +
-                        "}");
-        return getPrice() + healthyExtra1Price + healthyExtra2Price;
+        return super.itemizeHamburger() + healthyExtra1Price + healthyExtra2Price;
     }
-    
-    
+
+
 }
